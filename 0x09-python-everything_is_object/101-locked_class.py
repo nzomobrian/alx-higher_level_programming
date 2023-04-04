2ff1417ddc3d4466a4c1962985ee3336cdd21ae6
Nzomo <brnzomo17@gmail.com>
2023-04-04 22:36:42 +0300

    Module for a class that prevents dynamic attributes creation

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class LockedClass():
+    """Class to prevent dynamic attributes creation"""
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """Init method"""
+        pass
