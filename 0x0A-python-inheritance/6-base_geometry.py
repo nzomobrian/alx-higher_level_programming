5db63d79fcf30c0c8d856c1a684fcef87c52e8a1
Nzomo <brnzomo17@gmail.com>
2023-04-12 00:49:08 +0300

    a class BaseGeometry (based on 5-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains a basic geometry class"""
+
+
+class BaseGeometry:
+    """Basic geometry class"""
+
+    def area(self):
+        """Unimplemented area function"""
+        raise Exception("area() is not implemented")
