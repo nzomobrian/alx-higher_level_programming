8fd02f94b03a203d923dc255aa4a06f7fae6d732
Nzomo <brnzomo17@gmail.com>
2023-06-13 23:20:00 +0300

    Javascript Opening Phase

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-
-const newArr = list.map((item, idx) => item * idx);
-
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(newArr);
+console.log(list.map((item, index) => item * index));
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,17 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-fs.readFile(process.argv[2], (err, data) => {
-  if (err) throw err;
-
-  let out = data.toString();
-
-  fs.readFile(process.argv[3], (err, data) => {
-    if (err) throw err;
-
-    out += data.toString();
-    fs.writeFile(process.argv[4], out, (err) => {
-      if (err) throw err;
-    });
-  });
-});
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
